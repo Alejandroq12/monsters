3c2639d39562d778079288fd0afa17c46a906d1a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-18 16:19:26 -0600

    Feat(name): Dinamically add name.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Alejandro'
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi, {this.state.name}!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change name
+          </button>
         </header>
       </div>
     );
