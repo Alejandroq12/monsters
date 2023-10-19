69cd34ebbc872f1966425c8e47cffe9e74b6e199
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-19 15:04:39 -0600

    Feat(monsters): Render three characters.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,11 @@ class App extends Component {
     };
   }
   render() {
-    return <div className="App"></div>;
+    return <div className="App">
+      <h1>{this.state.monster1.name}</h1>
+      <h1>{this.state.monster2.name}</h1>
+      <h1>{this.state.monster3.name}</h1>
+    </div>;
   }
 }
 
