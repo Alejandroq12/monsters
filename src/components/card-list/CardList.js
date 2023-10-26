db378ceb79dbbacad0bc6679ad04543f83afb401
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-26 15:59:47 -0600

    Feat(Component): Add CardList.js component.

---
+++ src/components/card-list/CardList.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+  render() {
+    return(
+      <h1>Hello I'm the CardList Component</h1>
+      )
+    }
+  }
+
+export default CardList;
