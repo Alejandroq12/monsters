ecddc8170536c63a860d4c7dfde44bd5b43e3c57
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-26 15:20:39 -0600

    Feat(onSearchChange): Move onSearchChange into render and only calle it into the onChange event handler..

--- src/App.js
+++ src/App.js
@@ -33,8 +33,11 @@ class App extends Component {
 
   render() {
     console.log('render');
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     return (
       <div className="App">
@@ -42,7 +45,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
