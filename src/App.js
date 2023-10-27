a03e12305fa3f5c776b119bf89efc46b8f212354
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-27 00:17:38 -0600

    Fix: Erase unnecessary code.

--- src/App.js
+++ src/App.js
@@ -9,11 +9,9 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
-    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -32,7 +30,6 @@ class App extends Component {
   };
 
   render() {
-    console.log('render');
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
@@ -47,14 +44,8 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })} */}
-        <CardList />
+
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
