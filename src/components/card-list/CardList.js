3340b26cce0887324deac948cc3100eeeca97152
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-28 22:17:58 -0600

    Fix: Indent code properly.

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -3,14 +3,14 @@ import { Component } from 'react';
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
-    return(
+    return (
       <div>
-        {monsters.map(monster => (
+        {monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
       </div>
-      )
-    }
+    );
   }
+}
 
 export default CardList;
