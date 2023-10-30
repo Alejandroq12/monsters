153d8c8b854cc2e5a8a2cf6393d3b1f710dce13a
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-30 16:07:42 -0600

    Fix: Update class name.

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox
-          className='search-box'
+          className='monsters-search-box'
           onChangeHandler={onSearchChange}
           placeholder="search monsters"
         />
