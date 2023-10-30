cb4199a1589e84a03af16b25bdfe133b9fe52544
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-30 16:32:11 -0600

    Feat(cards): Use destructuring to get the name, email and id in a cleaner way.

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -7,16 +7,19 @@ class CardList extends Component {
     const { monsters } = this.props;
     return (
       <div>
-        {monsters.map((monster) => (
-          <div className="card-container" key={monster.id}>
-            <img
-              alt={`monster ${monster.name}`}
-              src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-            />
-            <h2>{monster.name}</h2>
-            <p>{monster.email}</p>
-          </div>
-        ))}
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className="card-container" key={id}>
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
