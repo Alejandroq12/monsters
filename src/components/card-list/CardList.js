182ba8be8b213f84b931079588b10b789630cb8e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-30 21:17:40 -0600

    Feat(card): Import card component.

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from '../card/Card'
 import './CardList.css';
 
 class CardList extends Component {
@@ -9,16 +10,8 @@ class CardList extends Component {
     return (
       <div className='card-list'>
         {monsters.map((monster) => {
-          const { name, email, id } = monster;
           return (
-            <div className="card-container" key={id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
+            <Card monster={monster}/>
           );
         })}
       </div>
