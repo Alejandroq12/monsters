0ce5d9cb0a91066feb52b1c4ff184f036e94b38e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-10-30 21:11:40 -0600

    Feat(card): Update code in order to import and use the card component, as well as to pass props to it.

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class App extends Component {
     });
     return (
       <div className="App">
+      <h1 className="app-title">Monsters</h1>
         <SearchBox
           className='monsters-search-box'
           onChangeHandler={onSearchChange}
