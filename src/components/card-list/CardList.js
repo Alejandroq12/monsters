b623f0e2d2c872d34d305a899d0755f5a187655d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-01 22:05:23 -0600

    Feat(card list): Migrate from classes to functional components.

--- src/components/card-list/CardList.js
+++ src/components/card-list/CardList.js
@@ -1,22 +1,12 @@
-import { Component } from 'react';
-import Card from '../card/Card'
+import Card from '../card/Card';
 import './CardList.css';
 
-class CardList extends Component {
-  render() {
-    console.log(this.props.monsters);
-    console.log('render from Card List');
-    const { monsters } = this.props;
-    return (
-      <div className='card-list'>
-        {monsters.map((monster) => {
-          return (
-            <Card key={monster.id} monster={monster}/>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
